f77c156dfa4443b55bb04a14322c571fcb212a6a
errante <00marcomaida00@gmail.com>
2018-08-22 13:33:05 +0200

    little fix

--- choco.bat
+++ choco.bat
@@ -40,7 +40,7 @@ ELSE (
 	echo ************************ 
 	timeout 8
 
-	REM @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+	@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 	start %~dpnx0 chocoisinstalled
 )
